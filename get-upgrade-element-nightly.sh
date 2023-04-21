43d042fd7f13186e5a00332e180d0ab78de62507
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2023-04-21 10:11:11 +0200

    Add detection for package already up to date

--- get-upgrade-element-nightly.sh
+++ get-upgrade-element-nightly.sh
@@ -3,6 +3,7 @@ set -o errexit
 RELEASE="https://packages.element.io/debian/dists/bullseye/Release"
 RELEASE_GPG="https://packages.element.io/debian/dists/bullseye/Release.gpg"
 PACKAGES="https://packages.element.io/debian/dists/bullseye/main/binary-amd64/Packages"
+PACKAGE_NAME="element-desktop-nightly-bin"
 
 function renderPKGBUILD() {
     DEB_PATH="$1"
@@ -13,7 +14,7 @@ LD_PRELOAD=/usr/lib/libsqlcipher.so exec "/opt/Element-Nightly/element-desktop-n
 SHEOF
 
 cat << EOF > PKGBUILD
-pkgname=element-desktop-nightly-bin
+pkgname=$PACKAGE_NAME
 pkgver=$VERSION
 pkgrel=1
 pkgdesc="All-in-one secure chat app for teams, friends and organisations (nightly .deb build)."
@@ -52,6 +53,12 @@ function main() {
         then
             DEB_PATH=$(awk '/^Filename:\s.*element-nightly_[0-9]+_amd64.deb/{print $2}' Packages)
             VERSION=$(echo "$DEB_PATH" | awk -F _ '{print $2}')
+            if pacman -Qi $PACKAGE_NAME | grep "$VERSION" > /dev/null
+            then
+                echo "Package $PACKAGE_NAME version $VERSION already up to date, exiting"
+                rm -r "$WD"
+                return 0
+            fi
             renderPKGBUILD "$DEB_PATH" "$VERSION"
             # Download the .deb file
             makepkg --nobuild
